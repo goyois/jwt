510b415f922a7a49a00cd47ca0a2331b29b4dd09
goyo <bau12288@gmail.com>
2023-03-16 16:40:14 +0900

    2강 기본인증 방식x

--- src/main/java/com/example/jwt/sucurity/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/sucurity/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.example.jwt.sucurity.config;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
 
 
 @Configuration
@@ -18,18 +19,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션 안씀
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 쿠키 세션 사용 x
                 .and()
                 .addFilter(corsFilter)  //필터가 cors 정책에서 벗어날 수 있으므로 cors 오류 해결
                 .formLogin().disable()  //jwt서버이기 떄문에 아이디 로그인을 폼 로그인으로 안함
-                .httpBasic().disable()
+                .httpBasic().disable()  //기본 인증 방식 x
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")  //해당 주소로 요청이 들어오면
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")  //user , admin 가능
                 .antMatchers("/api/v1/admin/**")
                 .access("hasRole('ROLE_ADMIN')") //admin만 가능
                 .anyRequest().permitAll();  //jwt 기본 config 설정
-
-
     }
 }
--- src/main/java/com/example/jwt/sucurity/controller/ApiController.java
+++ src/main/java/com/example/jwt/sucurity/controller/ApiController.java
@@ -1,6 +1,16 @@
 package com.example.jwt.sucurity.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class ApiController {
+
+    @GetMapping
+    public String home() {
+        return "<h1>home<h1>";
+    }
 }
